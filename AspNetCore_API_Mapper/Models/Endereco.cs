3e033013175fe36bdc0f323e489f4d1e18d2927e
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2021-06-01 01:34:09 -0300

    Criando a parte inicial do projeto mapper

---
+++ AspNetCore_API_Mapper/Models/Endereco.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_API_Mapper.Models
+{
+    public class Endereco
+    {
+
+    }
+}
